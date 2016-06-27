983b622354030c20b492eb9c6a5bbd8d3b269434
Casey Kelly <casey.kelly@colorado.edu>
2016-06-27 15:52:23 -0400

    add show method for employee struct type

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"strconv"
 )
 
 func main() {
@@ -24,6 +25,14 @@ type Employee struct {
 	Address string
 }
 
+// define show method for employee struct type
+func (employee *Employee) show() {
+	fmt.Println("Name: " + employee.Name)
+	fmt.Println("Serial: " + employee.Serial)
+	fmt.Println("Age: " + strconv.Itoa(employee.Age))
+	fmt.Println("Address: " + employee.Address)
+}
+
 func readFile(filename string, serial string) {
 	// read json file and save it to data variable
 	data, err := ioutil.ReadFile(filename)
@@ -41,7 +50,7 @@ func readFile(filename string, serial string) {
 	// check each employee struct for the one matching serial from command line flag
 	for i := 0; i < (len(employees)); i++ {
 		if employees[i].Serial == serial {
-			fmt.Printf("%+v\n", employees[i])
+			employees[i].show()
 		}
 	}
 }
