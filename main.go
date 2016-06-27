71849f097014415ae5f55e85dcb02cab95e10ca9
Casey Kelly <casey.kelly@colorado.edu>
2016-06-27 15:36:35 -0400

    add comments

--- main.go
+++ main.go
@@ -25,20 +25,20 @@ type Employee struct {
 }
 
 func readFile(filename string, serial string) {
-
+	// read json file and save it to data variable
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
+	// take slice of data and unmarshal it into slice of employee structs
 	jsonBlob := []byte(data)
 	var employees []Employee
 	err = json.Unmarshal(jsonBlob, &employees)
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-
+	// check each employee struct for the one matching serial from command line flag
 	for i := 0; i < (len(employees)); i++ {
 		if employees[i].Serial == serial {
 			fmt.Printf("%+v\n", employees[i])
