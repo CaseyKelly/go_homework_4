374ff797a3cb7a372017c93bdbc2d8b1625673bd
Casey Kelly <casey.kelly@colorado.edu>
2016-06-27 11:58:40 -0400

    capture flags from command line and feed into readFile function

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"flag"
+)
+
 func main() {
+	// Capture and parse flags from command line
+	serial := flag.String("serial", "0", "employee serial number")
+	filename := flag.String("filename", "0", "filename")
+	flag.Parse()
+
+	// Call function to fetch employee record
+	readFile(*filename, *serial)
+}
+
+func readFile(filename string, serial string) {
 
 }
