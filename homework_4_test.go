3eef45a855d4b7ef2d2243fbe87f61247eaee866
Casey Kelly <casey.kelly@colorado.edu>
2016-06-28 17:26:43 -0400

    add test file

---
+++ homework_4_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestHomework4(t *testing.T) {
+
+	Convey("Given the serial number of an employee", t, func() {
+
+		Convey("it should return the name of the employee", nil)
+
+		Convey("it should return the serial number of the employee", nil)
+
+		Convey("it should return the age of the employee", nil)
+
+		Convey("it should return the address of the employee", nil)
+
+	})
+
+}
